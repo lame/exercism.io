2a41e06882203699d3dcb8c660446ddfe207e1d2
Ryan Kuhl <rkk09c@gmail.com>
2014-10-08 16:01:06 -0400

    added exercises

--- hamming/hamming_test.py
+++ hamming/hamming_test.py
@@ -1,30 +1,30 @@
 import unittest
 
-import hamming
+from hamming import hamming
 
 
 class HammingTest(unittest.TestCase):
 
     def test_no_difference_between_identical_strands(self):
-        self.assertEqual(0, hamming.distance('A', 'A'))
+        self.assertEqual(0, hamming('A', 'A'))
 
     def test_complete_hamming_distance_of_for_single_nucleotide_strand(self):
-        self.assertEqual(1, hamming.distance('A', 'G'))
+        self.assertEqual(1, hamming('A', 'G'))
 
     def test_complete_hamming_distance_of_for_small_strand(self):
-        self.assertEqual(2, hamming.distance('AG', 'CT'))
+        self.assertEqual(2, hamming('AG', 'CT'))
 
     def test_small_hamming_distance(self):
-        self.assertEqual(1, hamming.distance('AT', 'CT'))
+        self.assertEqual(1, hamming('AT', 'CT'))
 
     def test_small_hamming_distance_in_longer_strand(self):
-        self.assertEqual(1, hamming.distance('GGACG', 'GGTCG'))
+        self.assertEqual(1, hamming('GGACG', 'GGTCG'))
 
     def test_large_hamming_distance(self):
-        self.assertEqual(4, hamming.distance('GATACA', 'GCATAA'))
+        self.assertEqual(4, hamming('GATACA', 'GCATAA'))
 
     def test_hamming_distance_in_very_long_strand(self):
-        self.assertEqual(9, hamming.distance('GGACGGATTCTG', 'AGGACGGATTCT'))
+        self.assertEqual(9, hamming('GGACGGATTCTG', 'AGGACGGATTCT'))
 
 
 if __name__ == '__main__':
